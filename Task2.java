acca2317fa773964905ceef0b7c9e9ad4ebdf2a8
Akash <dilliwalakash@gmail.com>
2020-11-09 12:10:25 -0500

    code update

--- Task2.java
+++ Task2.java
@@ -1,15 +1,12 @@
 class Task2 {
     public static void main (String args[]){
-
-    for(int i = 4; i >=0; i--){
+for(int i = 4; i >=0; i--){
 
     for(int j = i; j > 0; j--){
     System.out.print(" ");
     }
 
-    for(int k = 4-i; k >=0 ; k--)
-
-    
+    for(int k = 0; k <=4-i ; k++)
     System.out.print( k+1);
 
  System.out.println();
